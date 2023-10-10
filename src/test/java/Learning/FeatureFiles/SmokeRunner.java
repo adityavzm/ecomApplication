da56be52607518bb29314aacfd83dabeef96fda8
adityavzm <143366917+adityavzm@users.noreply.github.com>
2023-10-10 18:17:04 -0400

    Update SmokeRunner.java

--- src/test/java/Learning/FeatureFiles/SmokeRunner.java
+++ src/test/java/Learning/FeatureFiles/SmokeRunner.java
@@ -4,7 +4,7 @@ package Learning.FeatureFiles;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", monochrome=true, plugin= {"html: target/cucumber.html"}, tags="@smoke")
+@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", monochrome=true, plugin= {"html: target/cucumber-report-html/cucumber.html"}, tags="@smoke")
 public class SmokeRunner extends AbstractTestNGCucumberTests {
 	
 	
