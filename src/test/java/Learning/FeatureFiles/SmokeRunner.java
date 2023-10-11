e1048ff1325350a0fa5f5e89b76b1ad7f4f957b5
adityavzm <adityagav@gmail.com>
2023-10-11 11:34:54 -0400

    removed reports

--- src/test/java/Learning/FeatureFiles/SmokeRunner.java
+++ src/test/java/Learning/FeatureFiles/SmokeRunner.java
@@ -4,7 +4,8 @@ package Learning.FeatureFiles;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", monochrome=true, plugin= {"html: target/cucumber.html"}, tags="@smoke")
+@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", 	plugin = { "html:target/cucumber.html"},
+monochrome = true, tags="@smoke")
 public class SmokeRunner extends AbstractTestNGCucumberTests {
 	
 	
