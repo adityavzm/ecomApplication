d33846fa323480b6e178692bd250c9d1b41d98b7
adityavzm <143366917+adityavzm@users.noreply.github.com>
2023-10-13 11:39:47 -0400

    Generating multiple reports (#1)
    * Generating multiple reports
    
    * Added new feature

--- src/test/java/Learning/FeatureFiles/RegressionRunner.java
+++ src/test/java/Learning/FeatureFiles/RegressionRunner.java
@@ -4,7 +4,10 @@ package Learning.FeatureFiles;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", monochrome=true, plugin= {"html: target/cucumber.html"}, tags="@regression")
+@CucumberOptions(features="src/test/java/Learning/FeatureFiles",
+glue="Learning.stepDefinitions",
+plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json", "junit:target/cucumber-reports/Cucumber.xml", "html:target/cucumber-reports/Cucumber.html"}
+,monochrome = true, tags="@regression")
 public class RegressionRunner extends AbstractTestNGCucumberTests {
 	
 	
--- src/test/java/Learning/FeatureFiles/SmokeRunner.java
+++ src/test/java/Learning/FeatureFiles/SmokeRunner.java
@@ -4,8 +4,10 @@ package Learning.FeatureFiles;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/Learning/FeatureFiles",glue="Learning.stepDefinitions", 	plugin = { "html:target/cucumber.html"},
-monochrome = true, tags="@smoke")
+@CucumberOptions(features="src/test/java/Learning/FeatureFiles",
+				glue="Learning.stepDefinitions",
+				plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json", "junit:target/cucumber-reports/Cucumber.xml", "html:target/cucumber-reports/Cucumber.html"}
+				,monochrome = true, tags="@smoke")
 public class SmokeRunner extends AbstractTestNGCucumberTests {
 	
 	
