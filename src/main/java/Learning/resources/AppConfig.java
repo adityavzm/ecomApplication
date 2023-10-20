4e6a646c0e3c878907d96483ff81b297dd91449c
adityavzm <adityagav@gmail.com>
2023-10-19 20:12:44 -0400

    Added headless

--- src/main/java/Learning/resources/AppConfig.java
+++ src/main/java/Learning/resources/AppConfig.java
@@ -2,6 +2,6 @@ package Learning.resources;
 
 public class AppConfig {
 	
-	public static String browser = System.getProperty("browser","firefox");
+	public static String browser = System.getProperty("browser","headless");
 
 }
